721e42adc82c90bb31349929d0c8d0d933be92df
Ahmed_Arafat <63191956+Ahmed-Arafat10@users.noreply.github.com>
2021-02-16 16:31:32 +0200

    K-th lexicographical string of given length (BackTracking).cpp

--- K-th lexicographical string of given length.cpp
+++ K-th lexicographical string of given length.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+ll n , k ,cnt = 0;
+ll const N = 1e9;
+vector<char> arr(N);
+void BackTracking(ll x)
+{
+    if(cnt == k-1)
+    {
+        cout<<"FOUND: ";
+         cout<<arr[0]<<arr[1]<<arr[2]<<" "<<cnt<<"\n";
+         system("pause");
+    }
+    if(x == n)
+    {
+        cout<<arr[0]<<arr[1]<<arr[2]<<" "<<cnt<<"\n";
+        cnt++;
+        return;
+    }
 
+for(char c='a';c<='z';c++)
+{
+    arr[x] = c;
+    BackTracking(x+1);
+}
+
+}
+
+int main()
+{
+
+cin>>n>>k; 
+BackTracking(0);
+return 0;
+}
